ede32be93339f9cf11c24528c09cec1575e86890
ObsidianDragon <36376800+ObsidianDragon@users.noreply.github.com>
2023-03-25 11:44:13 -0400

    Add all 5 pages

---
+++ assets/scripts/btnHighlight.js
@@ -0,0 +1,9 @@
+var btns = document.querySelectorAll("#navlist > li > a")
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
