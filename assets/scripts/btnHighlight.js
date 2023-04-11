12975eccae85c0c328725345445e931557478e6b
ObsidianDragon <36376800+ObsidianDragon@users.noreply.github.com>
2023-04-11 17:09:19 -0400

    Add section backgrounds and button updates

--- assets/scripts/btnHighlight.js
+++ assets/scripts/btnHighlight.js
@@ -7,3 +7,38 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+function highlightMenuItem() {
+  // Get all menu items and corresponding sections
+  const menuItems = document.querySelectorAll('.btn');
+  const sections = document.querySelectorAll('.section');
+
+  // Find the section that is currently in view
+  let currentSection = null;
+  for (let i = 0; i < sections.length; i++) {
+    const section = sections[i];
+    const sectionTop = section.offsetTop;
+    const sectionBottom = sectionTop + section.offsetHeight;
+    if (window.pageYOffset >= sectionTop && window.pageYOffset < sectionBottom) {
+      currentSection = section;
+      break;
+    }
+  }
+
+  // Remove active class from all menu items
+  menuItems.forEach(item => item.classList.remove('active'));
+
+  // Add active class to the corresponding menu item
+  if (currentSection) {
+    const currentMenuItem = document.querySelector(`.btn[href="#${currentSection.id}"]`);
+    if (currentMenuItem) {
+      currentMenuItem.classList.add('active');
+    }
+  } else {
+    document.querySelector('.btn[href="#home"]').classList.add('active');
+  }
+}
+
+// Call the function on page load and scroll events
+document.addEventListener('DOMContentLoaded', highlightMenuItem);
+window.addEventListener('scroll', highlightMenuItem);
